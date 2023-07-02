1a9eb0ca3bd5a2694956dd4a60cb90628eb6718d
MrGreenNV <averkievnv@gmail.com>
2023-07-02 07:16:34 +0300

    init

---
+++ src/test/java/ru/averkiev/sensorclient/SensorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.averkiev.sensorclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
